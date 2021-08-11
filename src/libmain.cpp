8a5dfe2408be70bc186683f4c8322d5bbde235a8
cook1e <vincent.schweiger@xolley.de>
2021-08-11 12:57:30 +0200

    multi-platform

--- src/libmain.cpp
+++ src/libmain.cpp
@@ -1,5 +1,8 @@
-#include "Logger.hpp"
 #include "InjectionClient.hpp"
+#include "Logger.hpp"
+
+#ifdef __linux__
+
 #include <chrono>
 #include <stdio.h>
 #include <thread>
@@ -11,8 +14,7 @@ void f1() {
 }
 
 static void init(void) __attribute__((constructor));
-
-void start() {
+static void start() {
   Logger::logInfo("Starting...");
   InjectionClient();
   return;
@@ -20,5 +22,34 @@ void start() {
 
 static void init(void) {
   std::thread t(start);
-  t.join();
 }
+
+#elif _WIN32
+#include <Windows.h>
+using namespace std;
+void injectDLL() {
+  AllocConsole();
+  SetConsoleCtrlHandler(NULL, true);
+  FILE *fileIn;
+  FILE *fileOut;
+
+  freopen_s(&fileIn, "CONIN$", "r", stdin);
+  freopen_s(&fileOut, "CONOUT$", "w", stdout);
+  freopen_s(&fileOut, "CONOUT$", "w", stderr);
+  Logger::logSuccess("Injection succesfull");
+
+  InjectionClient();
+}
+
+BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+  if (fdwReason == DLL_PROCESS_ATTACH) {
+    CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)injectDLL, nullptr, 0,
+                 nullptr);
+  }
+
+  return TRUE;
+}
+#else
+#error Your OS is not supported
+#endif
+
